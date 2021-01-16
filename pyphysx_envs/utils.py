2d43b6f77087df14dd854ef4d063675ecb0d54a5
kzorina <ekaterina.zorina95@gmail.com>
2021-01-16 13:01:17 +0100

    Update follow_tool_tip_traj in utils.py

--- pyphysx_envs/utils.py
+++ pyphysx_envs/utils.py
@@ -56,41 +56,48 @@ from scipy.spatial.transform import Rotation as R
 from pyphysx_utils.transformations import multiply_transformations, inverse_transform, quat_from_euler
 from rlpyt_utils.utils import exponential_reward
 
-def follow_tool_tip_traj(env, poses):
+def follow_tool_tip_traj(env, poses, reward_to_track='spheres', add_end_steps=10):
     env.params['tool_init_position'] = poses[0]
     env.reset()
     action = np.random.normal(size=env._action_space.shape)
     for _ in range(5):
         _, rewards = env.step(action)
-    base_spheres = rewards['spheres']
-    # print("base spheres = ", base_spheres)
+    # base_spheres = 0
+    base_reward_to_track = rewards[reward_to_track]
+
     # print("Generating movement")
     # while env.renderer.is_active:
-    spheres_reward = 0
+    total_reward_to_track = 0
     traj_follow_reward = 0
-    for i in range(len(poses)):
-        desired_handle_pos, desired_handle_quat = multiply_transformations((poses[i][0], poses[i][1]),
-                                                                           inverse_transform(
-                                                                               env.scene.tool.to_tip_transform))
+    for i in range(len(poses) + add_end_steps):
+        # import time
+        # time.sleep(0.05)
+        demo_id = min(i, len(poses) - 1)
+        desired_handle_pos, desired_handle_quat = poses[demo_id][0], poses[demo_id][1]
+        # desired_handle_pos, desired_handle_quat = multiply_transformations((poses[demo_id][0], poses[demo_id][1]),
+        #                                                                    inverse_transform(
+        #                                                                        env.scene.tool.to_tip_transform))
 
-        tool_pos, tool_quat = env.scene.tool.get_global_pose()
-        lin_vel = (desired_handle_pos - tool_pos) / env.rate.period()
-        r1 = npq.as_rotation_matrix(desired_handle_quat).reshape((3, 3))
-        r0_inv = npq.as_rotation_matrix(tool_quat).reshape((3, 3)).T
-        r = npq.as_euler_angles(npq.from_rotation_matrix(r1.dot(r0_inv)))  # / env.rate.period()
-        r_r = R.from_matrix(r1.dot(r0_inv)).as_euler('xyz')
-        ang_vel = r_r / env.rate.period()
+        handle_pos, handle_quat = env.scene.tool.get_global_pose()
+        lin_vel = (desired_handle_pos - handle_pos) / env.rate.period()
+        # r1 = npq.as_rotation_matrix(desired_handle_quat).reshape((3, 3))
+        # r0_inv = npq.as_rotation_matrix(handle_quat).reshape((3, 3)).T
+        # r = npq.as_euler_angles(npq.from_rotation_matrix(r1.dot(r0_inv)))  # / env.rate.period()
+        # r_r = R.from_matrix(r1.dot(r0_inv)).as_euler('xyz')
+        # ang_vel = r_r / env.rate.period()
+        ang_vel = npq.as_rotation_vector(handle_quat ** (-1) * desired_handle_quat) / env.rate.period()
         _, rewards = env.step([*lin_vel, *ang_vel])
         # print(rewards)
         # for _ in range(2):
         #     env.step(np.zeros(env._action_space.shape))
         # print(rewards['spheres']- base_spheres)
         # print((rewards['spheres'] - base_spheres) / len(poses))
-        spheres_reward += (rewards['spheres'] - base_spheres) / len(poses)
+        # spheres_reward += 0
+        total_reward_to_track += (rewards[reward_to_track] - base_reward_to_track) / len(poses)
 
-        rewards['demo_positions'] = exponential_reward(tool_pos - desired_handle_pos, scale=0.5, b=10)
-        rewards['demo_orientation'] = exponential_reward([npq.rotation_intrinsic_distance(tool_quat, desired_handle_quat)],
+        rewards['demo_positions'] = exponential_reward(handle_pos - desired_handle_pos, scale=0.5, b=10)
+        rewards['demo_orientation'] = exponential_reward([npq.rotation_intrinsic_distance(handle_quat, desired_handle_quat)],
                                                          scale=0.5, b=1)
 
         traj_follow_reward += (rewards['demo_positions'] + rewards['demo_orientation']) / len(poses)
-    return spheres_reward, traj_follow_reward
+    return total_reward_to_track, traj_follow_reward
