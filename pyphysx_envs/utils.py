2ad8aab3ad7025fe0500d545e4406fc427e5068b
kzorina <ekaterina.zorina95@gmail.com>
2021-02-11 12:21:03 +0100

    Update utils in PyPhysx_utils

--- pyphysx_envs/utils.py
+++ pyphysx_envs/utils.py
@@ -56,7 +56,7 @@ from scipy.spatial.transform import Rotation as R
 from pyphysx_utils.transformations import multiply_transformations, inverse_transform, quat_from_euler
 from rlpyt_utils.utils import exponential_reward
 
-def follow_tool_tip_traj(env, poses, reward_to_track='spheres', add_end_steps=10):
+def follow_tool_tip_traj(env, poses, reward_to_track='spheres', add_end_steps=10, verbose=False):
     env.params['tool_init_position'] = poses[0]
     env.reset()
     action = np.random.normal(size=env._action_space.shape)
@@ -69,6 +69,7 @@ def follow_tool_tip_traj(env, poses, reward_to_track='spheres', add_end_steps=10
         # import time
         # time.sleep(0.05)
         demo_id = min(i, len(poses) - 1)
+        # print(f"at {demo_id} time is {(demo_id + 5) * env.rate.period()}")
         desired_handle_pos, desired_handle_quat = poses[demo_id][0], poses[demo_id][1]
         # desired_handle_pos, desired_handle_quat = multiply_transformations((poses[demo_id][0], poses[demo_id][1]),
         #                                                                    inverse_transform(
@@ -78,12 +79,18 @@ def follow_tool_tip_traj(env, poses, reward_to_track='spheres', add_end_steps=10
         lin_vel = (desired_handle_pos - handle_pos) / env.rate.period()
         ang_vel = npq.as_rotation_vector(desired_handle_quat * handle_quat.inverse()) / env.rate.period()
         _, rewards = env.step([*lin_vel, *ang_vel])
-        total_reward_to_track += (rewards[reward_to_track] - base_reward_to_track) / len(poses)
+
+        if verbose:
+            print(rewards)
+        if 'is_terminal' in rewards and rewards['is_terminal']:
+            return 0., 0.
+        total_reward_to_track += (rewards[reward_to_track] - base_reward_to_track) / (len(poses) + add_end_steps)
         # total_reward_to_track += (rewards[reward_to_track] - base_reward_to_track + rewards['box_displacement'])/ len(poses)
 
         rewards['demo_positions'] = exponential_reward(handle_pos - desired_handle_pos, scale=0.5, b=10)
         rewards['demo_orientation'] = exponential_reward([npq.rotation_intrinsic_distance(handle_quat, desired_handle_quat)],
                                                          scale=0.5, b=1)
 
-        traj_follow_reward += (rewards['demo_positions'] + rewards['demo_orientation']) / len(poses)
+        traj_follow_reward += (rewards['demo_positions'] + rewards['demo_orientation']) / (len(poses) + add_end_steps)
+
     return total_reward_to_track, traj_follow_reward
