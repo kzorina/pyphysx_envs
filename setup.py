b1b68b20b695c113d5177a9174397e9d39a69b41
kzorina <ekaterina.zorina95@gmail.com>
2021-02-17 14:20:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kzorina/pyphysx_envs",
     packages=setuptools.find_packages(),
-    package_data={'':['test.txt'], 'robot_data': ['*']},
+    package_data={'':['test.txt'],
+                  'robot_data': ['*.txt']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
