438b9cd46fd3799a24d26eab51f4cdc727bc4f6a
kzorina <ekaterina.zorina95@gmail.com>
2021-09-14 17:27:55 +0200

    Fix joint val example

--- pyphysx_envs/examples/06_follow_joint_val_sequence.py
+++ pyphysx_envs/examples/06_follow_joint_val_sequence.py
@@ -7,9 +7,13 @@ from matplotlib import pyplot as plt
 tool_name = 'spade'
 robot_name = 'panda'
 n_dof = 7
-rate = 50
-# joint_values = 0.01 * np.zeros((rate, n_dof))
-joint_values = 0.01 * np.ones((rate, n_dof))
+rate = 20
+# TODO: read joint_values from file
+# dummy data, joint value in range from 0 to ~pi/2
+dummy_per_joint = np.linspace(0., 1.57, num=100)
+joint_values = np.zeros((100, n_dof))
+for j in range(n_dof):
+    joint_values[:, j] = dummy_per_joint
 demo_params = {'goal_box_position': [1., 1., 0.],
                'sand_box_position': [0., 0., 0.]}
 
@@ -27,7 +31,8 @@ env = RobotEnv(scene_name=tool_name, tool_name=tool_name, robot_name=robot_name,
                )
                )
 env.reset()
-for i in range(joint_values.shape[0]):
-    _, r, _, _ = env.step(i * joint_values[i])
+for i in range(joint_values.shape[0] - 1):
+    velocity = (joint_values[i + 1] - joint_values[i]) * rate
+    _, r, _, _ = env.step(velocity)
 
 env.renderer.publish_animation()
