61249aaaa20e76d114e19663a353cc87fd7ab713
kzorina <ekaterina.zorina95@gmail.com>
2021-12-14 15:35:56 +0100

    Update utils.py

--- pyphysx_envs/utils.py
+++ pyphysx_envs/utils.py
@@ -4,6 +4,14 @@ from pyphysx_envs.scenes import HammerTaskScene, SpadeTaskScene, ScytheTaskScene
 from pyphysx_envs.tools import HammerTool, SpadeTool, ScytheTool
 from pyphysx_envs.robot import PandaRobot, TalosArmRobot, UR5
 
+from pyphysx_envs.envs import ToolEnv
+import numpy as np
+from os import path
+import pickle
+import quaternion as npq
+from scipy.spatial.transform import Rotation as R
+from pyphysx_utils.transformations import multiply_transformations, inverse_transform, quat_from_euler
+from rlpyt_utils.utils import exponential_reward
 
 def params_fill_default(params_default, params=None, add_noise=True, seed=None):
     # np.random.seed(31)
@@ -77,104 +85,62 @@ def get_robot(robot_name, **kwargs):
         raise NotImplementedError("Unknown robot '{}'".format(robot_name))
 
 
-from pyphysx_envs.envs import ToolEnv
-import numpy as np
-from os import path
-import pickle
-import quaternion as npq
-from scipy.spatial.transform import Rotation as R
-from pyphysx_utils.transformations import multiply_transformations, inverse_transform, quat_from_euler
-from rlpyt_utils.utils import exponential_reward
-
-
-def follow_tool_tip_traj(env, poses, rewards_to_track_name=('spheres'), add_end_steps=10, custom_hammer_return=False,
-                         verbose=False, return_tool_pose_list=True, save_to_file_name=None):
+def follow_tool_tip_traj(env, poses, rewards_to_track_name=('spheres'), add_end_steps=0, add_zero_end_steps=0,
+                         return_last_step_id=False, verbose=False, return_tool_pose_list=True,
+                         stop_on_positive_reward=False):
     env.params['tool_init_position'] = poses[0]
-    if custom_hammer_return:
-        nail_hammered_id = None
     env.reset()
     action = np.zeros((env._action_space.shape))
-    _, rewards = env.step(action)
+    _, rewards = env.step(action) # simulate one step to get initial reward
+    results = {}
     base_reward_to_track = sum([rewards[reward_to_track] for reward_to_track in rewards_to_track_name])
-    total_reward_to_track = 0
-    total_reward_to_track_list = []
-    traj_follow_reward = 0
-    tool_pose_list = []
+    # results['total_reward_to_track'] = 0
+    results['total_reward_to_track_list'] = []
+    results['traj_follow_reward'] = 0
+    results['tool_tip_pose_list'] = []
     for i in range(len(poses) + add_end_steps):
-        # import time
-        # time.sleep(0.05)
         demo_id = min(i, len(poses) - 1)
-        # print(f"at {demo_id} time is {(demo_id + 5) * env.rate.period()}")
-        # desired_handle_pos, desired_handle_quat = poses[demo_id][0], poses[demo_id][1]
-
-        # from video we reconstruct tip pose, but we can control only handle
+        if return_last_step_id:
+            results['last_step_id'] = demo_id
+        # from video we reconstruct tip pose, but the tool is controled by the handle
         desired_handle_pos, desired_handle_quat = multiply_transformations((poses[demo_id][0], poses[demo_id][1]),
                                                                            inverse_transform(
                                                                                env.scene.tool.to_tip_transform))
         handle_pos, handle_quat = env.scene.tool.get_global_pose()  # current pose
+        # compute the needed velocity based on difference between desired and real position
         lin_vel = (desired_handle_pos - handle_pos) / env.rate.period()  # required linear velocity
         ang_vel = npq.as_rotation_vector(desired_handle_quat * handle_quat.inverse()) / env.rate.period()  # required angular velocity
+        # apply velocity in the environment
         _, rewards = env.step([*lin_vel, *ang_vel])
-        tool_pose_list.append(multiply_transformations(env.scene.tool.get_global_pose(), env.scene.tool.to_tip_transform))
+        results['tool_tip_pose_list'].append(multiply_transformations(env.scene.tool.get_global_pose(),
+                                                                      env.scene.tool.to_tip_transform))
         if verbose:
             print(rewards)
+
         if 'is_terminal' in rewards and rewards['is_terminal']:
             if verbose:
                 print('Terminal reward obtained.')
-            if custom_hammer_return:
-                return 0., 0., -1
-            else:
-                return 0., 0.
-        # if 'is_terminal' in rewards and rewards['is_terminal']:
-        #     if 'spheres' in rewards_to_track_name and total_reward_to_track > 0:
-        #         if custom_hammer_return:
-        #             return total_reward_to_track, traj_follow_reward, i
-        #         if return_tool_pose_list:
-        #             return total_reward_to_track, traj_follow_reward, tool_pose_list[:i-1]
-        #         else:
-        #             return total_reward_to_track, traj_follow_reward
-        #     if verbose:
-        #         print('Terminal reward obtained.')
-        #     if custom_hammer_return:
-        #         return 0., 0., -1
-        #     else:
-        #         # print(i)
-        #         if return_tool_pose_list:
-        #             # return 0., 0., None
-        #             return 0., 0., []
-        #         else:
-        #             return 0., 0.
+            return results
 
         rewards['demo_positions'] = exponential_reward(handle_pos - desired_handle_pos, scale=0.5, b=10)
-        rewards['demo_orientation'] = exponential_reward([npq.rotation_intrinsic_distance(handle_quat, desired_handle_quat)],
-                                                         scale=0.5, b=1)
-        traj_follow_reward += (rewards['demo_positions'] + rewards['demo_orientation']) / (len(poses) + add_end_steps)
-        total_reward_to_track += (sum([rewards[reward_to_track] for reward_to_track in rewards_to_track_name]) - base_reward_to_track) / (len(poses) + add_end_steps)
-        # total_reward_to_track += (rewards[reward_to_track] - base_reward_to_track + rewards['box_displacement'])/ len(poses)
-        total_reward_to_track_list.append(total_reward_to_track)
-        # print(total_reward_to_track)
-        if save_to_file_name is not None:
-            pickle.dump(total_reward_to_track_list,
-                    open(save_to_file_name,'wb'))
-        if ('nail_hammered' in rewards_to_track_name or 'cutted_grass' in rewards_to_track_name) and total_reward_to_track > 0:
+        rewards['demo_orientation'] = exponential_reward([npq.rotation_intrinsic_distance(handle_quat,
+                                                                desired_handle_quat)], scale=0.5, b=1)
+        # store trajectory following reward divided by amount of steps (1. for perfect repeat)
+        results['traj_follow_reward'] += (rewards['demo_positions'] + rewards['demo_orientation']) / (len(poses) + add_end_steps)
+        results['total_reward_to_track_list'].append((sum(
+            [rewards[reward_to_track] for reward_to_track in rewards_to_track_name]) - base_reward_to_track) / (len(poses) + add_end_steps))
+
+        if stop_on_positive_reward and results['total_reward_to_track_list'].sum() > 0:
             scale_change = (len(poses) + add_end_steps) / max(i, 1)
-            # action = np.zeros((env._action_space.shape))
-            for _ in range(5):
+            for _ in range(add_zero_end_steps):
                 _, rewards = env.step(np.zeros((env._action_space.shape)))
-            if custom_hammer_return:
-                return scale_change * total_reward_to_track, scale_change * traj_follow_reward, i + 1
-            else:
-                if return_tool_pose_list:
-                    return scale_change * total_reward_to_track, scale_change * traj_follow_reward, tool_pose_list + [tool_pose_list[-1]]*5
-                else:
-                    return scale_change * total_reward_to_track, scale_change * traj_follow_reward
-    # for _ in range(5):
-    #     _, rewards = env.step(action)
-    if custom_hammer_return:
-        return total_reward_to_track, traj_follow_reward, len(poses) + add_end_steps - 1
-    else:
-        if return_tool_pose_list:
-            return total_reward_to_track, traj_follow_reward, tool_pose_list
-        else:
-            return total_reward_to_track, traj_follow_reward
+                results['tool_tip_pose_list'].append(multiply_transformations(env.scene.tool.get_global_pose(),
+                                                                              env.scene.tool.to_tip_transform))
+                results['traj_follow_reward'] += (rewards['demo_positions'] + rewards['demo_orientation']) / (
+                            len(poses) + add_end_steps)
+                results['total_reward_to_track_list'].append((sum(
+                    [rewards[reward_to_track] for reward_to_track in rewards_to_track_name]) - base_reward_to_track) / (
+                                                                         len(poses) + add_end_steps))
+            return results
+    return results
 
