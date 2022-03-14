00e398a72024ad5c1942055c34b41a1b0a113f45
kzorina <ekaterina.zorina95@gmail.com>
2022-03-14 17:23:38 +0100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyphysx_envs",
-    version="0.5",
+    version="0.6",
     author="Kateryna Zorina",
     author_email="zorina.kateryna.mail@gmail.com",
     description="Package contains a set of environments created with pyphysx library.",
